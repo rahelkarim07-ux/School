2607934f99698ce6ccbbebb1a37157a5d9c66d0a
Rahel Karim <rahel.karim07@yahoo.fi>
2026-08-11 09:05:41 +0300

    Update scirpt.js

---
+++ scirpt.js
@@ -0,0 +1,14 @@
+// this is the best code
+
+function add(a, b) {
+    return a + b;
+}
+
+if (require.main === module) {
+    const assert = require('assert');
+    assert.strictEqual(add(2, 3), 5, 'add should return the sum of two numbers');
+    assert.strictEqual(add(-1, 1), 0, 'add should handle negative numbers');
+    console.log('All tests passed');
+}
+
+module.exports = { add };
